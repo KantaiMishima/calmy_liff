8c477d9e9be103488954066cc16dbda8d288da20
KantaiMishima <k6knti@gmail.com>
2019-09-29 13:47:50 +0900

    open icon

--- src/MainMap/MainMap.jsx
+++ src/MainMap/MainMap.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, AppBar, Toolbar } from '@material-ui/core';
-import img from "./img.png";
+import img from "./image.png";
+import InsertEmoticonTwoToneIcon from '@material-ui/icons/InsertEmoticonTwoTone';
+import SentimentVeryDissatisfiedTwoToneIcon from '@material-ui/icons/SentimentVeryDissatisfiedTwoTone';
+
+/** アイコンのポジション */
+const tirePosition = {
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: '32%',
+    left: '30%',
+}
 
 function MainMap() {
-  return (
-    <Grid container direction="column">
-        <AppBar position="static">
-            <Toolbar>トイレ空き状況</Toolbar>
-        </AppBar>
-        <img alt="" style={{ maxWidth: '100vw' }} src={img} ></img>
-    </Grid>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <Grid container direction="column">
+            <AppBar position="static">
+                <Toolbar>トイレ空き状況</Toolbar>
+            </AppBar>
+            <Grid style={{ position: 'relative' }} container direction="column">
+                <img alt="" style={{ width: '100vw' }} src={img} ></img>,
+                {isOpen ? <InsertEmoticonTwoToneIcon fontSize="large" color="primary" style={tirePosition} /> : <SentimentVeryDissatisfiedTwoToneIcon fontSize="large" color="secondary" style={tirePosition} />}
+            </Grid>
+        </Grid>
+    );
 }
 
 export default MainMap;
