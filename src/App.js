130d63ba4f989e63552b959cbd8c649744f58a23
KantaiMishima <mishimakantai@gmail.com>
2020-05-24 14:27:33 +0900

    carete menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Grid, makeStyles } from "@material-ui/core";
 import './App.css';
 
+const useStyles = makeStyles(()=>{
+  return {
+    container: {
+      height: "100vh"
+    },
+    item: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      backgroundColor: "lightBlue",
+      border: "solid gray 1px"
+    }
+  };
+});
+
 function App() {
+  const classes = useStyles();
 
   return (
     <div className="App">
+      <Grid container className={classes.container}>
+        <Grid item xs={6} className={classes.item}>親しい友達登録</Grid>
+        <Grid item xs={6} className={classes.item}>部屋を見る</Grid>
+      </Grid>
     </div>
   );
 }
